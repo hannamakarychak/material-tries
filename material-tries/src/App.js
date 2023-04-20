43064ab686861682cf469acea95c8377a6ddb325
hannamakarychak <annamakarichak3@gmail.com>
2023-04-20 20:28:50 +0200

    adds card

--- material-tries/src/App.js
+++ material-tries/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
+import { Grid } from '@mui/material';
+import { Container } from '@mui/system';
 import './App.css';
+import MovieCard from './components/movie-card/movie-card';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Grid container spacing={3}>
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+        </Grid>
+
+      </Container>
+
     </div>
   );
 }
---
+++ material-tries/src/components/movie-card/movie-card.js
@@ -0,0 +1,12 @@
+import { Card, Grid } from '@mui/material';
+
+const MovieCard = () => {
+  return <Grid item xs={3}>
+    <Card>
+      <img src="https://ex-fs.net/uploads/posts/2015-01/1422289660_b88e9d6b26.jpg" alt="sex and the city" className='img' />
+
+    </Card>
+  </Grid>
+};
+
+export default MovieCard;
