2d0c63a255da6b5e06e7a79ad32ee7693705b635
hannamakarychak <annamakarichak3@gmail.com>
2023-04-21 18:53:05 +0200

    adds header

--- material-tries/src/App.js
+++ material-tries/src/App.js
@@ -2,12 +2,14 @@
 import { Grid } from '@mui/material';
 import { Container } from '@mui/system';
 import './App.css';
+import ButtonAppBar from './components/button-app-bar/button-app-bar';
 import MovieCard from './components/movie-card/movie-card';
 
 function App() {
   return (
     <div className="App">
-      <Container>
+      <ButtonAppBar />
+      <Container sx={{ marginY: 6 }}>
         <Grid container spacing={3}>
           <MovieCard />
           <MovieCard />
@@ -15,7 +17,6 @@ function App() {
           <MovieCard />
         </Grid>
       </Container>
-
     </div>
   );
 }
