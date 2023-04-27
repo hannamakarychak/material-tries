533e885e07fdbde5343752cb2b558fb34dc4f010
hannamakarychak <annamakarichak3@gmail.com>
2023-04-27 12:05:17 +0200

    fetches data from open database

--- material-tries/src/App.js
+++ material-tries/src/App.js
@@ -1,21 +1,15 @@
 
-import { Grid } from '@mui/material';
 import { Container } from '@mui/system';
-import './App.css';
 import ButtonAppBar from './components/button-app-bar/button-app-bar';
-import MovieCard from './components/movie-card/movie-card';
+import List from './components/list/list';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <ButtonAppBar />
       <Container sx={{ marginY: 6 }}>
-        <Grid container spacing={3}>
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-        </Grid>
+        <List />
       </Container>
     </div>
   );
